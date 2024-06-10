326c6689005d0708f99ec12cbe739d8918ea57e8
hamza_dahir <hamza.dahir@hotmail.com>
2024-06-10 23:57:02 +0100

    init

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Tofis Changes")
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Jericho is Leng')
 
 
 # Press the green button in the gutter to run the script.
